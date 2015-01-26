11e2dac9552f21eaae731206beeecabf54356f3a
harishbbio <harishb@Harishs-MacBook-Pro-4.local>
2015-01-25 16:07:15 -0800

    Updated my version of the Programming Assignment 2
    Have two functions that can create matrix and returns the inverse of
    that matrix using lexical scooping rules.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function creates matrix and the second function calculates the inverse of the matrix in the cache.
 
-## Write a short comment describing this function
+## The first function, makeCacheMatrix creates a matrix ‘x’, which is really a list containing ## a function to
+##
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse matrix
+## get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    y <- NULL
+    set <- function(y) {
+    x <<- y
+    y <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) y <<- solve
+  getinverse <- function() y
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## CacheSolve function returns a matrix that is the inverse of matrix 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  y <- x$getinverse()
+  if(!is.null(y)) {
+    message("getting cached data")
+    return(y)
+  }
+  data <- x$get()
+  y <- solve(data)
+  x$setinverse(y)
+  y
 }
